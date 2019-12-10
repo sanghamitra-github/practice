fa55d69256443b2e58445303751f413d175bce61
esaaray <sanghamitra.ray@ericsson.com>
2019-12-10 17:07:42 +0530

    modified project

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -3,7 +3,7 @@ package com.test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		for(int i=0; i<=10; i++) {
+		for(int i=0; i<=5; i++) {
 			System.out.println("HellowWorld....count:"+i);
 		}
 		System.out.println("After successful execution");
