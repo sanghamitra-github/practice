f9c323882940ef728497b8e0b7f5c473c9f2908a
esaaray <sanghamitra.ray@ericsson.com>
2019-12-10 16:23:30 +0530

    added project

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		for(int i=0; i<=10; i++) {
+			System.out.println("HellowWorld....count:"+i);
+		}
+		System.out.println("After successful execution");
+	}
+
+}
