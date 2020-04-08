557b2c68251f62d1b07b138cc804ac2c05b310ce
esaaray <sanghamitra.ray@ericsson.com>
2020-04-08 14:42:33 +0530

    modification on 8th April 2020

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -2,11 +2,24 @@ package com.test;
 
 public class HelloWorld {
 
+	private static final Object[] Array = null;
+
 	public static void main(String[] args) {
-		for(int i=0; i<=5; i++) {
+		for(int i=0; i<=20; i++) {
 			System.out.println("HellowWorld....count:"+i);
 		}
 		System.out.println("After successful execution");
+		
+		/*
+		 * // array of 3 int[] num = new int[3];
+		 * 
+		 * num[0] = 1; num[1] = 2; num[2] = 3; num[3] = 4;
+		 * //ArrayIndexOutOfBoundsException: 3
+		 * 
+		 * System.out.println("num[0] : " + num[0]); System.out.println("num[1] : " +
+		 * num[1]); System.out.println("num[2] : " + num[2]);
+		 * System.out.println("num[3] : " + num[3]); //ArrayIndexOutOfBoundsException: 3
+		 */
 	}
 
 }
